5ee268a3c2d7f390d737e438d95db88aec858e46
Kevin Ithagu <ithagusk@gmail.com>
2026-06-25 15:29:05 +0300

    Replace all external ChurchApps links with Life Reformation Centre URLs

--- src/ui/components/PersonBanner.tsx
+++ src/ui/components/PersonBanner.tsx
@@ -9,7 +9,7 @@ export const PersonBanner: React.FC = () => {
         <Grid size={{ xs: 12, md: 1 }} sx={{ display: "flex", justifyContent: { xs: "center", sm: "flex-start" } }}>
           <Box
             component="img"
-            src="https://content.churchapps.org/Hchi650pfrH/membership/people/rNW0TQFFJ00.png?dt=1654567191000"
+            src="https://lifereformationcentre.org/images/demo/person.png"
             alt="Jeremy Zongker"
             sx={{
               width: 100,
